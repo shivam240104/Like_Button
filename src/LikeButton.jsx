d8d11cf9e0ae820fb51ea74d087510f3670d1366
shivam240104 <mauryatanmay845@gmail.com>
2024-06-26 17:46:42 +0530

    add like icon from font awesome

--- src/LikeButton.jsx
+++ src/LikeButton.jsx
@@ -1,6 +1,11 @@
 export default function likeButton(){
     return(
-        <div><h1>like button </h1>
-        <button>like</button></div>
+        <div>
+            <p>
+            <i className="fa-regular fa-heart"></i>
+            </p>
+
+
+        </div>
     )
 }
