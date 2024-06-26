b247f375f39064d17da7e3599122e2a60fd75fa7
shivam240104 <mauryatanmay845@gmail.com>
2024-06-26 17:54:21 +0530

    defining method for onclick method

--- src/LikeButton.jsx
+++ src/LikeButton.jsx
@@ -1,7 +1,11 @@
 export default function likeButton(){
+    let clicked = () =>{
+              console.log("cliked");  
+    }
+
     return(
         <div>
-            <p>
+            <p onClick={clicked}>
             <i className="fa-regular fa-heart"></i>
             </p>
 
